2b6bc12806f50b4cd5ddab4b4ba177d8c29f23cc
Niyati <youremail@example.com>
2026-05-12 01:21:45 +0530

    feat: add views folder with EJS templates

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,20 +1,16 @@
-const socket = io();//connection starts(not http)
-//get user info 
+const socket = io();
 const session = JSON.parse(sessionStorage.getItem('mm_session') || '{}');
 
 if (session.role === 'admin') {
-  //send admin updates
   socket.emit('join-admin');
-  //server sends
   socket.on('new-order', (data) => {
-    //shows pop
     showSocketToast(`🛒 New order! ₹${data.totalPrice} — ${data.time}`, 'var(--accent)');
     if (typeof loadOrders === 'function') loadOrders();
   });
 } else if (session.role === 'student') {
   socket.emit('join-student', session.id);
   socket.on('order-status-update', (data) => {
-    showSocketToast(` ${data.message}`, 'var(--accent2)');
+    showSocketToast(`📦 ${data.message}`, 'var(--accent2)');
   });
 }
 
